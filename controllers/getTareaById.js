7dcda9e9c2e637f68785f0f333ff728bd3761ac8
milaCa <cami.stapff@gmail.com>
2024-07-05 11:27:35 -0300

    error solucionado

--- controllers/getTareaById.js
+++ controllers/getTareaById.js
@@ -3,7 +3,7 @@ import { ModeloTarea } from "../database/Models/ModeloTarea.js";
 export const getTareaById = (req, res, next) => {
   const idTarea = req.params.id;
 
-  ModeloTarea.findOne({ id: idTarea, logueado: req.usuario.id })
+  ModeloTarea.findOne({ id: idTarea, usuario: req.usuario.id })
     .then((data) => {
       if (!data) {
         throw new Error(`No existe ninguna tarea con el Id: ${idTarea}`);
